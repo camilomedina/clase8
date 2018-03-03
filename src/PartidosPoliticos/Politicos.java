a03eda56cf6d0a4706286cb7d331cb4af7ac9526
Unknown <camilomedina1508@gmail.com>
2018-03-03 08:39:19 -0500

    commit

--- src/PartidosPoliticos/Politicos.java
+++ src/PartidosPoliticos/Politicos.java
@@ -4,34 +4,23 @@
  * and open the template in the editor.
  */
 package PartidosPoliticos;
-import java.math.BigDecimal;
+
 /**
  *
  * @author Familia Díaz Rendón
  */
 public class Politicos {
-    
-    public String nombre;
-    public String apellido;
-    public int documento;
-    public int edad;
-    public BigDecimal telefono;
-    private String direccion;
-    private String ciudadNacimiento;
-    public String partidoPolitico;
-    
+
+    private String partidoPolitico;
+    private double valorCampaña;
+    private int voto;
     public Politicos(){
     
     }
-    public Politicos(String nombre,String apellido,int documento,int edad,BigDecimal telefono,String direccion,String ciudadNacimiento,String partidoPolitico){
-       this();
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.documento=documento;
-        this.edad=edad;
-        this.telefono=telefono;
-        this.direccion=direccion;
-        this.ciudadNacimiento=ciudadNacimiento;
-        this.partidoPolitico=partidoPolitico;
+    Politicos(String _partidoPolitico,double _valorCampaña, int _voto){
+       this.partidoPolitico=_partidoPolitico;
+       this.valorCampaña=_valorCampaña;
+       this.voto=_voto;
+
     }
 }
