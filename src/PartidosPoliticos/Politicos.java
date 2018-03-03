7712c6cc4222496112884713f7a197b3c77f68d9
Unknown <cd--55@hotmail.com>
2018-03-03 07:36:35 -0500

    Commit Inicial

---
+++ src/PartidosPoliticos/Politicos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PartidosPoliticos;
+import java.math.BigDecimal;
+/**
+ *
+ * @author Familia Díaz Rendón
+ */
+public class Politicos {
+    
+    public String nombre;
+    public String apellido;
+    public int documento;
+    public byte edad;
+    public BigDecimal telefono;
+    
+    public Politicos(){
+    
+    }
+}
---
+++ src/Principal/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+import PartidosPoliticos.Politicos;
+/**
+ *
+ * @author Familia Díaz Rendón
+ */
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Politicos polo= new Politicos();
+        Politicos liberal= new Politicos();
+        Politicos verde= new Politicos();
+        Politicos mira= new Politicos();
+        System.out.println("_______________________________");
+        System.out.println("1.Informacion Electoral");
+        System.out.println("2.Votar");
+        System.out.println("_______________________________");
+        
+    }
+    
+}
